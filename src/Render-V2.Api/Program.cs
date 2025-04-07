45ec7bcf035de6b2b6a1d6ef52b35053d34409fd
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-08 01:27:06 +0500

    create

--- src/Render-V2.Api/Program.cs
+++ src/Render-V2.Api/Program.cs
@@ -2,11 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer().AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseSwagger().UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
